150cd8007db735052aa378678ff57d3800a6f88f
micksems <ys125@fordham.edu>
2026-08-23 17:22:27 -0400

    Show application timing metrics on job dashboard

--- src/app/job-search/page.js
+++ src/app/job-search/page.js
@@ -1,10 +1,16 @@
 import { cookies } from "next/headers";
 import DashboardClient from "./DashboardClient";
+import TimingWidget from "./TimingWidget";
 import { jobSearchData } from "@/data/jobSearchData";
 
 export const dynamic = "force-dynamic";
 
 export default async function JobSearchPage() {
   await cookies();
-  return <DashboardClient jobSearchData={jobSearchData} />;
+  return (
+    <>
+      <TimingWidget jobs={jobSearchData.jobs} />
+      <DashboardClient jobSearchData={jobSearchData} />
+    </>
+  );
 }
