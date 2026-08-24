4c7be80eb0c61e46c4a780acd5b5d3aa30b4286b
micksems <ys125@fordham.edu>
2026-08-24 17:04:54 -0400

    Simplify career dashboard surface

--- src/app/job-search/page.js
+++ src/app/job-search/page.js
@@ -1,16 +1,10 @@
 import { cookies } from "next/headers";
 import DashboardClient from "./DashboardClient";
-import TimingWidget from "./TimingWidget";
 import { jobSearchData } from "@/data/jobSearchData";
 
 export const dynamic = "force-dynamic";
 
 export default async function JobSearchPage() {
   await cookies();
-  return (
-    <>
-      <TimingWidget jobs={jobSearchData.jobs} />
-      <DashboardClient jobSearchData={jobSearchData} />
-    </>
-  );
+  return <DashboardClient jobSearchData={jobSearchData} />;
 }
